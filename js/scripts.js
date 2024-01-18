d0f17e553245599e8e7ab4c9280cfb271b8ed1d1
weraseemann <wera.seemann@gmail.com>
2024-01-18 14:44:22 +0100

    created a basic To-Do-List app
    Created a basic To-Do-List app with the following features:
    
    The user can add a new item to a list of items.
    The user can cross out an item from the list of items.
    The user can delete an item from the list of items.
    The user can change the order of items in the list of items.

---
+++ js/scripts.js
@@ -0,0 +1,39 @@
+
+function newItem(){
+
+    //jQuery
+    //1. Adding a new item to the list of items: 
+       let li = $('<li></li>');
+       let inputValue = $('#input').val();
+       li.append(inputValue);
+    
+       if (inputValue === '') {
+         alert("You must write something!");
+       } else {
+         $('#list').append(li);
+       }
+    
+     //2. Crossing out an item from the list of items:
+       function crossOut() {
+             li.toggleClass('strike');
+         }
+    
+         li.on("dblclick",function (crossOut){
+        li.toggleClass('strike');
+      });
+    
+     //3(i). Adding the delete button "X": 
+       let deleteButton = $('<crossOutButton></crossOutButton>');
+         deleteButton.append(document.createTextNode('X'));
+         li.append(deleteButton);
+    
+         deleteButton.on('click', function(deleteListItem)
+     //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
+       {
+             li.addClass('delete');
+         });
+     // 4. Reordering the items: 
+       $('#list').sortable();
+    
+    }
+    
